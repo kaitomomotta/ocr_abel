fbabf57f26c95aeb019dbe5686d5ecfff894c477
Abel Roinsard <104033433+kaitomomotta@users.noreply.github.com>
2022-10-16 11:58:54 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <err.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+int main( int argc, char** argv ) {
+	/* Make your program do whatever you want */
+    printf("non non non\n\noui");
+}
